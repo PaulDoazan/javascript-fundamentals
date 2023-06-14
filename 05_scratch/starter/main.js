3ac8624612211758cb29bc13dfb081701223a169
pdoazan <paul.doazan@gmail.com>
2023-06-14 15:33:19 +0200

    Add Mosaic correction in scratch project

--- 05_scratch/starter/main.js
+++ 05_scratch/starter/main.js
@@ -38,34 +38,53 @@
 
 
 // 2. Alterner 2 couleurs du container avec un click
+// const container = document.querySelector('.container')
+// const mainContainer = document.querySelector('.main-container')
+// const btn = document.querySelector('.btn')
+
+// const newDiv = document.createElement("div");
+// newDiv.classList.add('new-div')
+// mainContainer.appendChild(newDiv)
+
+// const newDiv2 = document.createElement("div");
+// newDiv2.classList.add('new-div')
+// mainContainer.appendChild(newDiv2)
+// newDiv2.style.left = "10vw"
+// newDiv2.style.backgroundColor = "green"
+
+// btn.addEventListener('click', onBtnClick)
+
+// let changeColorToBlack = true;
+// let originColor = container.style.backgroundColor;
+
+// function onBtnClick() {
+//     if (changeColorToBlack) {
+//         container.style.backgroundColor = 'black'
+//     } else {
+//         container.style.backgroundColor = originColor
+//     }
+
+//     changeColorToBlack = !changeColorToBlack
+// }
+
+// Implémenter la fonction pour créer une mosaique de div qui remplit tout le main-container, 10 colomnes et 10 lignes, couleur au choix
 const container = document.querySelector('.container')
 const mainContainer = document.querySelector('.main-container')
 const btn = document.querySelector('.btn')
 
-const newDiv = document.createElement("div");
-newDiv.classList.add('new-div')
-mainContainer.appendChild(newDiv)
+btn.addEventListener('click', onBtnClick)
 
-const newDiv2 = document.createElement("div");
-newDiv2.classList.add('new-div')
-mainContainer.appendChild(newDiv2)
-newDiv2.style.left = "10vw"
-newDiv2.style.backgroundColor = "green"
-
-btn.addEventListener('mouseenter', onBtnClick)
-btn.addEventListener('mouseleave', onBtnClick)
-
-let changeColorToBlack = true;
-let originColor = container.style.backgroundColor;
+const colors = ['yellow', 'red']
 
 function onBtnClick() {
-    if (changeColorToBlack) {
-        container.style.backgroundColor = 'black'
-    } else {
-        container.style.backgroundColor = originColor
+    for (let line = 0; line < 10; line++) {
+        for (let col = 0; col < 10; col++) {
+            const newDiv = document.createElement("div");
+            mainContainer.appendChild(newDiv)
+            newDiv.classList.add('new-div')
+            newDiv.style.left = `${col}0vw`
+            newDiv.style.top = `${line}0vh`
+            newDiv.style.backgroundColor = colors[(col + line) % 2]
+        }
     }
-
-    changeColorToBlack = !changeColorToBlack
 }
-
-// Implémenter la fonction pour créer une mosaique de div qui remplit tout le main-container, 10 colomnes et 10 lignes, couleur au choix

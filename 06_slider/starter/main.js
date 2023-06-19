4c075526651a3edc30ede808f2c88f286b6a1a7b
pdoazan <paul.doazan@gmail.com>
2023-06-19 14:21:52 +0200

    Add features slider 06

--- 06_slider/starter/main.js
+++ 06_slider/starter/main.js
@@ -14,9 +14,20 @@ init();
 btns.forEach((e, index) => {
     e.leftOrRight = index;
     e.addEventListener("click", onClickBtn);
+    e.addEventListener("mousedown", onDownBtn);
+    e.addEventListener("mouseup", onUpBtn);
 });
 
+function onDownBtn(e) {
+    console.log('down')
+}
+
+function onUpBtn(e) {
+    console.log('up')
+}
+
 function onClickBtn(e) {
+    console.log('click')
     if (e.currentTarget.leftOrRight === 0) {
         slideIndex--;
     } else {
@@ -39,12 +50,18 @@ function onClickBtn(e) {
     displayImages(slideIndex)
 }
 
-function displayImages(ind) {
-
+function displayImages(slideI) {
+    imgs.forEach((img, indexImg) => {
+        img.style.transition = 'transform 3s';
+        img.style.transform = `translateX(${(indexImg - slideI) * 100}%)`
+    })
 }
 
 function init() {
     slideIndex = 0;
     btns[0].style.opacity = 0.5
     btns[0].style.pointerEvents = 'none'
+    imgs.forEach((img, indexImg) => {
+        img.style.transform = `translateX(${(indexImg - slideIndex) * 100}%)`
+    })
 }

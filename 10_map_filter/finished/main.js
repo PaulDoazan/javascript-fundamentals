4f161be171d08f28e30d302061abb09bd4faa905
pdoazan <paul.doazan@gmail.com>
2023-06-21 21:28:01 +0200

    Correct map_filter exercices

--- 10_map_filter/finished/main.js
+++ 10_map_filter/finished/main.js
@@ -1,96 +1,168 @@
 import movies from "./movies.json" assert { type: 'json' };
-import games from "./games.json" assert { type: 'json' };
-
-init()
-
-function init() {
-
-    let result = getTitlesByDirector(movies, 'Christopher Nolan')
-    console.log(result);
-}
 
 // a. Logger le premier element du tableau movies
-// --> console.log(movies[0]);
-// b. Logger l'annee du 4eme element du tableau movies
-// --> console.log(movies[3].year);
-// c. Logger le titre du dernier element du tableau movies
-// --> console.log(movies[movies.length - 1].title);
-// d. Logger le titre du film qui a la meilleure note
-// function getBestElement(arr) {
-//     let bestMovie = arr[0]
-//     // for (let i = 0; i < arr.length; i++) {
-//     //     if (arr[i].rate > bestMovie.rate) {
-//     //         bestMovie = arr[i]
-//     //     }
-//     // }
+// console.log(movies[0])
 
-//     arr.forEach((el) => {
-//         if (el.rate > bestMovie.rate) {
-//             bestMovie = el;
-//         }
-//     })
-//     return bestMovie.title;
+// b. Logger l'annee du 4eme element du tableau movies
+// console.log(movies[3].year)
+
+// c. Logger le titre du dernier element du tableau movies
+// console.log(movies[movies.length - 1].title)
+
+// d. Logger le titre du film qui a la meilleure note, solution alternative avec .sort() / .reduce()
+// let bestMovie = movies[0]
+
+// movies.forEach((movie) => {
+//     if(movie.rate >= bestMovie.rate){
+//         bestMovie = movie
+//     }
+// })
+
+// // console.log(bestMovie.title)
+
+// // alternative .sort()
+// movies.sort((a, b) => {
+//     return b.rate - a.rate;
+// })
+
+// console.log(movies[0].title)
+
+// e. Logger le titre du film le plus ancien, solution alternative avec .sort()
+// let oldestMovie = movies[0]
+
+// movies.forEach((currentElement) => {
+//     if(currentElement.year < oldestMovie.year){
+//         oldestMovie = currentElement
+//     }
+// })
+
+// console.log(oldestMovie.title)
+
+// alternative .sort()
+
+// movies.sort((a, b) => {
+//     return b.year - a.year
+// })
+
+// console.log(movies[0].title)
+
+// e.bis calculer la somme totale des éléments du tableau
+// let arr = [1, 12, 8, 2, 5]
+// let total = 0;
+
+// // arr.forEach((currentNumber) => {
+// //     total += currentNumber
+// // })
+
+// // console.log(total)
+
+// const logElementFlechee = (element) => {
+//     console.log(`Mon élément à afficher dans une fonction fléchée : ${element}`)
 // }
 
-// e. Logger le titre du film le plus ancien
-function getOldestMovie(arr) {
-    let oldestMovie = arr[0]
+// total = 12
 
-    arr.forEach((el) => {
-        if (el.year < oldestMovie.year) {
-            oldestMovie = el;
-        }
+// arr.forEach((curr) => {
+//     total += curr
+// })
+
+// let arrStr = ['Bye', 'Tchüss', 'Ciao', 'Au revoir']
+
+// let sentence = ''
+
+// arrStr.forEach((word, ind) => {
+//     if(ind === arrStr.length - 1){
+//         sentence += word
+//     } else {
+//         sentence += word + ', '
+//     }
+// })
+
+// console.log(sentence)
+// console.log(total)
+
+// f. Logger tous les titres de film qui ont au moins 3 acteurs / .filter() avec un .map() / .reduce()
+// let movieTitles = []
+
+// movies.forEach((currentElement) => {
+//     if(currentElement.actors.length >= 3){
+//         movieTitles.push(currentElement.title)
+//     }
+// })
+
+// const filteredMovies = movies.filter((el) => {
+//     return el.actors.length >= 3
+// })
+
+// const filteredMovieTitles = filteredMovies.map((currentElement) => {
+//     return currentElement.title
+// })
+
+// console.log(filteredMovieTitles)
+
+// const arr = ['hello -- english', 'salut -- français', 'bye -- english', 'au revoir -- français', 'Guten tag -- deutsch']
+
+// const newArr = movies.map((movie) => {
+//     return movie.title + ' -- super film'
+// })
+
+// function filterByLanguage(lang, array) {
+//     const filteredArr = array.filter((el) => {
+//         return el.includes(lang)
+//     })  
+
+//     return filteredArr
+// }
+
+// const result = filterByLanguage('italiano', arr)
+
+// let users = [{name: 'Paul', role: 'user'}, {name: 'Rihab', role: 'admin'}, {name: 'Laurian', role: 'admin'}, {name: 'Céline', role: 'client'}]
+
+// const filteredUsers = users.filter((user) => {
+//     return user.role === 'client'
+// })
+// console.log(filteredUsers)
+
+// 1. Ecrire une fonction qui prend en parametre le tableau movies et qui retourne un nouveau tableau de tous les titres de films / .map()
+// function getAllTitles(arr){
+//     const mappedArr = arr.map((element) => {
+//         return element.title
+//     })
+
+//     return mappedArr;
+// }
+
+// const result = getAllTitles(movies)
+
+// console.log(result)
+// 2. Ecrire une fonction qui prend en parametre une annee et un tableau (movies), et qui retourne un nouveau tableau de titres de film de l'annee specifiee / .filter()
+// function getMoviesByYear(annee, arr){
+//     const filteredArray = arr.filter((element) => {
+//         return element.year === annee
+//     })
+
+//     const mappedFilteredArray = filteredArray.map((el) => {
+//         return el.title
+//     })
+
+//     return mappedFilteredArray;
+// }
+
+// const res = getMoviesByYear(1994, movies)
+
+// console.log(res)
+
+// 3. Ecrire une fonction qui prend en parametre un realisateur et un tableau (movies), et qui retourne un nouveau tableau de tous les titres de films de ce realisateur / .map()
+function getMoviesByDirector(directorName, arr) {
+    let filteredArr = arr.filter((el) => {
+        return el.director === directorName
     })
 
-    return oldestMovie.title
-}
-// f. Logger tous les titres de film qui ont au moins 3 acteurs
-
-function getMovieWithAtLeastThreeActors(arr) {
-    let result = []
-    arr.forEach((el) => {
-        if (el.actors.length >= 3) {
-            result.push(el.title)
-            console.log(el.title)
-        }
+    let mappedArr = filteredArr.map((el) => {
+        return el.title
     })
-
-    return result;
+    return mappedArr
 }
+const result = getMoviesByDirector('Christopher Nolan', movies)
 
-// 1. Ecrire une fonction qui prend en parametre le tableau movies et qui retourne un nouveau tableau de tous les titres de films
-function getTitlesFromArray(arr) {
-    return arr.map(el => el.title)
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     titlesArr.push(arr[i].title)
-    // }
-}
-
-// 2. Ecrire une fonction qui prend en parametre une annee et un tableau (movies), et qui retourne un nouveau tableau de titres de film de l'annee specifiee
-function getTitlesFromArrayByYear(arr, year) {
-    let arrTitleByYear = []
-    arr.forEach((element) => {
-        if (element.year === year) arrTitleByYear.push(element.title)
-    })
-
-    return arrTitleByYear;
-    // let titlesByYear = []
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i].year === year) {
-    //         titlesByYear.push(arr[i].title)
-    //     }
-    // }
-    // return titlesByYear;
-}
-
-// 3. Ecrire une fonction qui prend en parametre un realisateur et un tableau (movies), et qui retourne un nouveau tableau de tous les titres de films de ce realisateur
-function getTitlesByDirector(arr, name) {
-    let elementsByDirector = []
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].director === name) {
-            elementsByDirector.push(arr[i].title)
-        }
-    }
-    return elementsByDirector;
-}
+console.log(result)
